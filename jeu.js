8e2846fd6611ae6537f72e54eaa8542910d45866
aboalsim114 <aboalsim114@gmail.com>
2023-05-23 15:41:13 +0200

    test

--- jeu.js
+++ jeu.js
@@ -2,13 +2,16 @@ import Accueil from "./Acceuil.js"
 
 const LARGEUR_FENETRE = 750; // Définition de la largeur de la fenêtre du jeu
 const HAUTEUR_FENETRE = 600; // Définition de la hauteur de la fenêtre du jeu
-const VELOCITE_LATERAL_VAISSEAU = 300; // Définition de la vitesse latérale (horizontale) du vaisseau du joueur
-const VELOCITE_HORIZONTALE_VAISSEAU = 250; // Définition de la vitesse horizontale du vaisseau du joueur
+var VELOCITE_LATERAL_VAISSEAU = 300; // Définition de la vitesse latérale (horizontale) du vaisseau du joueur
+var VELOCITE_HORIZONTALE_VAISSEAU = 250; // Définition de la vitesse horizontale du vaisseau du joueur
+
+
 const VELOCITE_HORIZONTALE_BULLET = 700; // Définition de la vitesse horizontale des projectiles tirés par le vaisseau
 const VELOCITE_BOMB = 500
 const VELOCITE_ENNEMI = 200; // Définition de la vitesse des ennemis
 const INTERVALLE_ENNEMI = 2000; // Définition de l'intervalle de temps entre chaque création d'un nouvel ennemi
 const INTERVALLE_BOMB = 5000;
+const INTERVALLE_PowerUp = 8000;
 
 var ENEMIES_DE_SUITE // vatiable qui permet de savoir combien d'ennemié d'affilé ont été tuée
 var SCORE; // Initialisation du score à 0
@@ -38,6 +41,11 @@ class Home extends Phaser.Scene {
         this.comboText = null // Variable pour stocker le nombre de vie
         this.isPaused = false; // Variable pour indiquer si le jeu est en pause ou non
         this.coinsText = null;
+        this.hasPowerUp = false;
+        this.PowerUp = null;
+        this.powerUpTimer = null;
+        this.powerUpDuration = 10000;
+
         this.bg = Phaser.GameObjects.tileSprite;
     }
 
@@ -50,7 +58,8 @@ class Home extends Phaser.Scene {
         this.load.audio("bulletSound", "audio/shoot.wav");
         this.load.audio("explosionSound", "audio/expolosion1.wav");
         this.load.image("enemy", "image/enemy.png");
-        this.load.image("coins", "image/coins.png")
+        this.load.image("coins", "image/coins.png");
+        this.load.image("powerUp", "image/powerup.png");
         // Charge une feuille de sprites pour l'animation d'explosion à partir de l'image "explosion.png"
         this.load.spritesheet("explosion", "image/explosion.png", {
             frameWidth: 64,
@@ -70,7 +79,7 @@ class Home extends Phaser.Scene {
         const gameOverText = this.add.text(
             this.cameras.main.width / 2,
             this.cameras.main.height / 2,
-            'Game Over', {fontSize: '32px', fill: '#fff'}
+            'Game Over', { fontSize: '32px', fill: '#fff' }
         );
         gameOverText.setOrigin(0.5);
         this.physics.pause(); // Met  jeu en pause
@@ -101,6 +110,7 @@ class Home extends Phaser.Scene {
 
 
     create() {
+
         multiplicateur = 1;
         ENEMIES_DE_SUITE = 0;
         NB_VIE = 3;
@@ -108,7 +118,7 @@ class Home extends Phaser.Scene {
         SCORE = 0;
         BEST_SCORE = localStorage.getItem("bestS");
         coins = localStorage.getItem("coins")
-        const {width, height} = this.scale;
+        const { width, height } = this.scale;
         this.add.image(0, 0, "bg"); // Ajoute une image de fond
         this.bg = this.add.tileSprite(0, 0, width, height, "bg").setScale(2)
         this.add.image(LARGEUR_FENETRE - 20, 30, "heart"); // Ajoute une image du nombre de vie restantes
@@ -120,14 +130,14 @@ class Home extends Phaser.Scene {
             BEST_SCORE = 0; // Si aucun score n'est stocké, initialise le score à 0
         }
 
-        this.best_scoreText = this.add.text(10, 40, 'Record: ' + BEST_SCORE, {fontSize: '24px', fill: '#ffffff'}) // Variable pour stocker le texte affichant le score du joueur
-        this.scoreText = this.add.text(10, 10, 'Score: ' + SCORE, {fontSize: '24px', fill: '#ffffff'}); // Utilise this.score pour afficher le score
-        this.vieText = this.add.text(LARGEUR_FENETRE - 60, 15, NB_VIE, {fontSize: '24px', fill: '#ffffff'});
+        this.best_scoreText = this.add.text(10, 40, 'Record: ' + BEST_SCORE, { fontSize: '24px', fill: '#ffffff' }) // Variable pour stocker le texte affichant le score du joueur
+        this.scoreText = this.add.text(10, 10, 'Score: ' + SCORE, { fontSize: '24px', fill: '#ffffff' }); // Utilise this.score pour afficher le score
+        this.vieText = this.add.text(LARGEUR_FENETRE - 60, 15, NB_VIE, { fontSize: '24px', fill: '#ffffff' });
         this.comboText = this.add.text(LARGEUR_FENETRE / 2 - 50, 15, "Combo :x" + multiplicateur, {
             fontSize: '24px',
             fill: '#ffffff'
         })
-        this.coinsText = this.add.text(LARGEUR_FENETRE - 70, 68, coins, {fontSize: '24px', fill: '#ffffff'});
+        this.coinsText = this.add.text(LARGEUR_FENETRE - 70, 68, coins, { fontSize: '24px', fill: '#ffffff' });
         this.cursors = this.input.keyboard.createCursorKeys(); // Crée les touches du clavier pour le contrôle du vaisseau
         this.bullets = this.physics.add.group(); // Crée un groupe de projectiles
         this.vaisseau = this.physics.add.image(LARGEUR_FENETRE / 2, HAUTEUR_FENETRE, "vaisseau"); // Ajoute le vaisseau du joueur
@@ -137,21 +147,23 @@ class Home extends Phaser.Scene {
         this.input.keyboard.on('keydown-SPACE', this.fireBullet, this); // Associe la touche d'espace à la fonction de tir des projectiles
         this.enemies = this.physics.add.group(); // Crée un groupe pour les ennemis
         this.bombs = this.physics.add.group(); // Crée un groupe pour les bombs
+        this.PowerUp = this.physics.add.group();
         this.startEnemyTimer(); // Démarre le minuteur pour la création des ennemis
         this.startBombTimer(); // Démarre le minuteur pour la création des ennemis
-
+        this.startPowerUpTimer();
         this.physics.add.overlap(this.vaisseau, this.enemies, this.enemyCollision, null, this); // Gère la collision entre le vaisseau et les ennemis
+        this.physics.add.overlap(this.vaisseau, this.PowerUp, this.collectPowerUp, null, this); // Gère la collision entre le vaisseau et les powerUp
         this.input.keyboard.on('keydown-P', this.togglePause, this); // Associe la touche P à la fonction de mise en pause/reprise du jeu
         this.pauseText = this.add.text(
             LARGEUR_FENETRE / 2,
             HAUTEUR_FENETRE / 2,
-            'Jeu en pause', {fontSize: '32px', fill: '#ffffff'}
+            'Jeu en pause', { fontSize: '32px', fill: '#ffffff' }
         ); // Ajoute un texte pour afficher l'état de pause du jeu
         this.pauseText.setOrigin(0.5); // Définit l'origine du texte de pause
         this.pauseText.setVisible(false); // Masque le texte de pause par défaut
         this.anims.create({
             key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion", {start: 0, end: 15}),
+            frames: this.anims.generateFrameNumbers("explosion", { start: 0, end: 15 }),
             frameRate: 24,
             repeat: 0,
             hideOnComplete: true,
@@ -163,6 +175,7 @@ class Home extends Phaser.Scene {
 
 
     update() {
+
         this.bg.tilePositionY -= 3;
         if (NB_VIE >= 0) {
             this.vieText.setText(NB_VIE); // Met à jour le texte affichant le score
@@ -211,6 +224,11 @@ class Home extends Phaser.Scene {
 
         this.physics.overlap(this.bullets, this.enemies, this.bulletEnemyCollision, null, this); // Gère la collision entre les projectiles et les ennemis
         this.physics.overlap(this.bullets, this.bombs, this.bulletBombCollision, null, this);
+        this.physics.overlap(this.vaisseau, this.PowerUp, this.collectPowerUp, null, this);
+        console.log(this.hasPowerUp);
+        if (this.hasPowerUp) {
+            this.activatePowerUp();
+        }
 
     }
 
@@ -277,6 +295,82 @@ class Home extends Phaser.Scene {
 
         }
     }
+
+
+
+    activatePowerUp() {
+        if (this.hasPowerUp) {
+            return;
+        }
+        const power = ["speed"];
+        const random = Phaser.Math.RND.pick(power);
+        console.log(random);
+        switch (random) {
+            case "speed":
+                this.activateSpeed();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    collectPowerUp(vaisseau, powerUp) {
+        powerUp.destroy();
+        this.activatePowerUp()
+
+    }
+
+
+    createPowerUp() {
+        const x = Phaser.Math.Between(0, LARGEUR_FENETRE);
+        const y = -10;
+
+        const powerUp = this.PowerUp.create(x, y, "powerUp");
+        powerUp.setVelocityY(VELOCITE_BOMB);
+
+    }
+
+    activateSpeed() {
+        this.vaisseau.setVelocityX(-VELOCITE_LATERAL_VAISSEAU * 2);
+        this.vaisseau.setVelocityY(-VELOCITE_HORIZONTALE_VAISSEAU * 2);
+
+        this.hasPowerUp = true;
+
+        this.time.delayedCall(10000, () => {
+            this.resetSpeed();
+            this.hasPowerUp = false;
+        });
+    }
+
+
+
+
+
+
+    resetSpeed() {
+        this.hasPowerUp = false;
+        this.vaisseau.setVelocityX(VELOCITE_LATERAL_VAISSEAU);
+        this.vaisseau.setVelocityY(VELOCITE_HORIZONTALE_VAISSEAU);
+    }
+
+
+
+
+
+
+    startPowerUpTimer() {
+        this.powerUpTimer = this.time.addEvent({
+            delay: INTERVALLE_PowerUp, // Délai entre chaque création d'powerUp
+            callback: this.createPowerUp, // Fonction de création de l'powerUp
+            callbackScope: this, // Portée du callback (cette scène)
+            loop: true // Indique que l'événement doit se répéter en boucle
+        });
+    }
+
+
+
+
     miseAjourMulti() {
         if (ENEMIES_DE_SUITE > 5) {
             if (ENEMIES_DE_SUITE > 10) {
@@ -359,7 +453,7 @@ const config = {
     physics: {
         default: "arcade",
         arcade: {
-            gravity: {y: 0}
+            gravity: { y: 0 }
         }
     },
     scene: [Accueil, Home]
